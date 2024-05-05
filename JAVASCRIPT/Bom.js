bf992facd30c9b62f1d738834d2945743700b581
Nagarjuna Yadavalli <86463176+Nagarjunay123@users.noreply.github.com>
2024-05-05 10:23:13 +0530

    Add files via upload

---
+++ JAVASCRIPT/Bom.js
@@ -0,0 +1,80 @@
+//! innerHeight and outerHeight
+// console.log(window.innerHeight)
+// console.log(window.outerHeight)
+
+//! innerWidth and outerWidth
+// console.log(window.innerWidth)
+// console.log(window.outerWidth)
+
+//! Session storage
+// console.log(sessionStorage)
+//* setting values for session
+// sessionStorage.setItem("name","nag")
+// sessionStorage.setItem("age",23)
+
+//* getting values for session
+// let name=sessionStorage.getItem("name")
+// console.log(name)
+
+//! History
+// console.log(history.length)
+
+// function navigateForward(){
+//     history.forward()
+// }
+
+// function navigateBackward(){
+//     history.back()
+// }
+
+// function navigateMove(){
+//     history.go(2)
+// }
+
+// function navigateBack(){
+//     history.go(-2)
+// }
+
+//! Screen
+// console.log(screen)
+
+// console.log(screen.height)
+// console.log(screen.width)
+
+// console.log(screen.availHeight)
+// console.log(screen.availWidth)
+
+// console.log(screen.colorDepth)
+// console.log(screen.pixelDepth)
+
+
+//! location
+// console.log(location)
+// console.log(location.hostname)
+// console.log(location.host)
+// console.log(location.protocol)
+// console.log(location.port)
+// console.log(location.hash)
+// console.log(location.href)
+// console.log(location.origin)
+
+// let navigate=()=>{
+//     location.reload()
+// }
+
+// let change1=()=>{
+//     location.assign("https://w3schools.com")
+// }
+
+// let change2=()=>{
+//     location.replace("https://w3schools.com")
+// }
+
+//! navigator
+console.log(navigator)
+console.log(navigator.appName)
+console.log(navigator.appCodeName)
+console.log(navigator.languages)
+console.log(navigator.language)
+console.log(navigator.cookieEnabled)
+console.log(navigator.onLine)
